07f39f9071c153623c1e9a44b1f2706beff67f55
stawbreedelight <emmaofthefishes@gmail.com>
2026-08-23 12:47:48 -0700

    Create app.js

---
+++ Public/Date-planner/app.js
@@ -0,0 +1,10 @@
+const createButton = document.getElementById('createButton');
+const joinButton = document.getElementById('joinButton');
+
+createButton.addEventListener('click', () => {
+    alert('Create a Date is working!');
+});
+
+joinButton.addEventListener('click', () => {
+    alert('Join a Date is working!');
+});
